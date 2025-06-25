70de36d01e03b5914314f4300c4114122dcaf70b
Aditya Johansah <146818051+ivepyife@users.noreply.github.com>
2025-06-25 14:43:28 +0700

    jenis aset (#9) (#10)
    Co-authored-by: Kezia Natalia Wongkar <112630994+kezianw@users.noreply.github.com>

---
+++ database/migrations/2025_06_24_151020_create_jenis_asets_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jenis_asets', function (Blueprint $table) {
+             $table->id();
+            $table->foreignId('kelompok_aset_id')->constrained()->onDelete('cascade');
+            $table->foreignId('akun_aset_id')->constrained()->onDelete('cascade');
+            $table->string('kode_jenis_aset')->unique();
+            $table->string('nama_jenis_aset')->unique();
+            $table->string('kode', 50)->default('');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jenis_asets');
+    }
+};
